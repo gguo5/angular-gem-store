7a1cd22cc709297f67d609c2723cb2fb4fffec3f
Grant <grant_guo@dataagility.com>
2014-11-10 14:10:12 +1100

    b4 refactoring product tabs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,36 @@
 angular.module('angularGemStoreApp')
   .controller('MainCtrl', function () {
   })
+  .directive('productDescription',function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'views/product-description.html'
+    };
+  })
+  .directive('productSpecs',function(){
+    return {
+      restrict: 'A',
+      templateUrl: 'views/product-specs.html'
+    };
+  })
+  .directive('productTabs',function(){
+    return {
+      restrict: 'E',
+      templateUrl: 'views/product-tabs.html',
+      controller: function () {
+      this.tab = 1;
+
+      this.setTab = function(newValue){
+        this.tab = newValue;
+      };
+
+      this.isSet = function(tabName){
+        return this.tab === tabName;
+      };
+    },
+    controllerAs: 'tab'
+    };
+  })
   .controller('StoreController', function () {
     this.products = gems;
   })
